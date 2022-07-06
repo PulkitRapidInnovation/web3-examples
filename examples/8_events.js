3dd5dce16410113f3294a59acd04f788ce6f5d65
Pulkit <pg@rapidinnovation.dev>
2022-07-06 20:20:03 +0530

    fix web3 js event issue

--- examples/8_events.js
+++ examples/8_events.js
@@ -1,8 +1,10 @@
 const Web3 = require('web3')
-const rpcURL = 'https://ropsten.infura.io/v3/844f6a66a00049ed92fc80bd433495e4' 
-// const rpcURL = 'wss://HTTP://127.0.0.1:7545'
+// const rpcURL = 'https://ropsten.infura.io/v3/844f6a66a00049ed92fc80bd433495e4' 
+const rpcURL = 'HTTP://127.0.0.1:7545'
 const web3 = new Web3(rpcURL)
-const abi=[
+
+const address='0x4605dB866b18a401153266D4c13A44c45f999872'
+const abi =[
 	{
 		"inputs": [
 			{
@@ -42,34 +44,54 @@ const abi=[
 		"type": "event"
 	}
 ]
-async function init(){
-    const address = '0x350411A2Dfc243e35fCF71a702136b4B2B35D22b'
-    // const addresses = web3.eth.getAccounts();
-    // console.log("🚀 ~ file: 8_events.js ~ line 46 ~ addresses", addresses)
-    const contract = new web3.eth.Contract(abi, address)
-    
-    const eventOptions = { filter: {}, fromBlock: 1253010, toBlock: 'latest' };
-const events = await contract.getPastEvents('MyEvent', eventOptions);
 
-contract.events.MyEvent(async function (error, result) {
-    console.log(result,error)
-});
-    // const receipt = contract.methods.emitEvent('hey').send({
-    //     from:'0x80e411e422607d4373bB05dF1606353D255e70d5'
+const init = async () =>{
+	const addresses = await web3.eth.getAccounts();
+console.log("🚀 ~ file: 8_events.js ~ line 126 ~ addresses", addresses)
+	const contract = await new web3.eth.Contract(abi,address);
+    // FIRST WAY--------------------------------------------------------------------- current event 
+	// const receipt = await contract.methods.emitEvent('hey').send({
+    //     from:addresses[0]
     // });
-    // console.log("🚀 ~ file: 8_events.js ~ line 53 ~ receipt ~ receipt", receipt)
+    //console.log("🚀 ~ file: 8_events.js ~ line 133 ~ receipt ~ receipt", receipt.events)
     
-    // const results = await contract.getPastEvents('MyEvent',{})
-    // console.log("🚀 ~ file: 8_events.js ~ line 54 ~ results", results)
-    // console.log(receipt.events)
-    
-    // contract.events.MyEvent({}).on('data',event=>console.log(event))
-    // contract.events.MyEvent({
-    
-    // }, function(error, event){ console.log(event,error); })
-    // .on('data', function(event){
-    //     console.log(event); // same results as the optional callback above
-    // })
+	//END First way --------------------------------------------------------------------------
+	
+
+
+
+
+
+	//Second way -----------------------------------------------------------------------------------
+	// await contract.methods.emitEvent('hey').send({
+	// 	    from:addresses[0]
+	// 	});
+    // await contract.methods.emitEvent('hey hey').send({
+	// 	    from:addresses[0]
+	// 	});
+		
+	// 	const results = await contract.getPastEvents('MyEvent',{fromBlock:0})
+    //     console.log("🚀 ~ file: 8_events.js ~ line 67 ~ init ~ results", results)
+
+	//END Second way -----------------------------------------------------------------------------------
+
+
+
+
+		await contract.methods.emitEvent('hey').send({
+		    from:addresses[0]
+		});
+		await contract.events.MyEvent({}).on('data',event=>console.log(event));
+
+		//await new Promise(resolve => setTimeout(()=>resolve),2000);
+		var delayInMilliseconds = 2000; //1 second
+
+setTimeout(function() {
+  //your code to be executed after 1 second
+}, delayInMilliseconds);
+		await contract.methods.emitEvent('hey hey').send({
+		    from:addresses[0]
+		});
 }
 
 init()
